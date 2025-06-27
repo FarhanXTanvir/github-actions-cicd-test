40ebfdeaa2f569413364cc019c7074e226004897
Farhan Tanvir <64136272+FarhanXTanvir@users.noreply.github.com>
2025-06-28 01:56:10 +0600

    Test Github CI/CD

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+export default function App() {
+  return (
+    <div
+      style={{
+        display: "grid",
+        placeItems: "center",
+        height: "100vh",
+      }}
+    >
+      <button
+        style={{
+          color: "red",
+          fontSize: "20px",
+          backgroundColor: "lightblue",
+          padding: "10px",
+          border: "2px solid black",
+          borderRadius: "5px",
+          textAlign: "center",
+          boxShadow: "2px 2px 5px rgba(0,0,0,0.3)",
+          transition: "all 0.3s ease",
+          cursor: "pointer",
+        }}
+      >
+        Github Actions, CI/CD Test
+      </button>
+    </div>
+  );
+}
